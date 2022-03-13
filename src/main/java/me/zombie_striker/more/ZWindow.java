612a02a06285c96cb30349c9fa1580e353d24e35
xsmeths <joshii797@gmail.com>
2022-03-13 21:49:11 +0000

    start adding more to the Zwindow class

--- src/main/java/me/zombie_striker/more/ZWindow.java
+++ src/main/java/me/zombie_striker/more/ZWindow.java
@@ -7,11 +7,13 @@ public class ZWindow extends Canvas implements Runnable {
 
     private JFrame window;
     private Thread thread;
+    private JPanel panel;
 
-
-    public ZWindow(String name){
+    public ZWindow(String name, int locX, int locY, int width, int height){
         window = new JFrame(name);
-        window.setSize(1000,1000);
+        panel = new JPanel();
+        window.setSize(width,height);
+        window.setLocation(locX,locY);
         window.setResizable(false);
         window.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
     }
